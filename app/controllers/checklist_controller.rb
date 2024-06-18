09b6465699411a6da50f4ef87fff603a4719ed3b
Samuel Lam <samuellam123456@hotmail.com>
2024-06-18 22:42:04 +0800

    Revert "Merge branch 'feature/checklist' of https://github.com/Service-Design-Studio/1d-final-project-summer-2024-sds-2024-team-10 into feature/checklist"
    This reverts commit 9caa5c050c7ab340905c71b0db094f99ec405cd4, reversing
    changes made to 30b47380855cd551f3c0ada04ed4847b6b7f97af.

--- app/controllers/checklist_controller.rb
+++ app/controllers/checklist_controller.rb
@@ -1,20 +1,4 @@
 class ChecklistController < ApplicationController
     def checklist
     end
-
-    def passport
-    end
-
-    def proof_of_employment
-    end
-
-    def proof_of_residential_address
-    end
-
-    def proof_of_tax_residency
-    end
-
-    def proof_of_mobile_ownership
-    end
-    
 end
