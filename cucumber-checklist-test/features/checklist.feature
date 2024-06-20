583e20ecb44b569fb9341206797a779d6900d015
SahityaPettugani <iamsahitya20@gmail.com>
2024-06-20 18:04:35 +0800

    final

--- cucumber-checklist-test/features/checklist.feature
+++ cucumber-checklist-test/features/checklist.feature
@@ -1,12 +1,44 @@
 Feature: Document Checklist Form
+  Scenario: Next button becomes clickable when all checkboxes are checked
+    Given I am on the checklist page
+    And I see the following checklist items:
+      | Document                    | Status   |
+      | Passport                    | checked  |
+      | Proof of Employment         | checked  |
+      | Proof of Residential Address| checked  |
+      | Proof of Tax Residency      | checked  |
+      | Proof of Mobile Ownership   | checked  |
+    Then the Next button should be clickable and red
 
   Scenario: Next button is grayed out when some checkboxes are unchecked
     Given I am on the checklist page
     And I see the following checklist items:
-      | Passport                   | unchecked |
-      | Employment                 | checked   |
-      | Address                    | unchecked |
-      | Tax                        | checked   |
-      | Mobile                     | unchecked |
-      
+      | Document                    | Status   |
+      | Passport                    | checked  |
+      | Proof of Employment         | unchecked|
+      | Proof of Residential Address| unchecked|
+      | Proof of Tax Residency      | unchecked|
+      | Proof of Mobile Ownership   | unchecked|
+    Then the Next button should be grayed out
+
+  Scenario: Next button is grayed out when some checkboxes are unchecked
+    Given I am on the checklist page
+    And I see the following checklist items:
+      | Document                    | Status   |
+      | Passport                    | checked  |
+      | Proof of Employment         | checked  |
+      | Proof of Residential Address| unchecked|
+      | Proof of Tax Residency      | unchecked|
+      | Proof of Mobile Ownership   | unchecked|
+    Then the Next button should be grayed out
+
+  Scenario: Next button is grayed out when some checkboxes are unchecked
+    Given I am on the checklist page
+    And I see the following checklist items:
+      | Document                    | Status   |
+      | Passport                    | checked  |
+      | Proof of Employment         | unchecked|
+      | Proof of Residential Address| unchecked|
+      | Proof of Tax Residency      | checked  |
+      | Proof of Mobile Ownership   | unchecked|
     Then the Next button should be grayed out
--- cucumber-checklist-test/features/step_definitions/checklist_steps.rb
+++ cucumber-checklist-test/features/step_definitions/checklist_steps.rb
@@ -1,9 +1,11 @@
 # features/step_definitions/checklist_steps.rb
-
 require 'selenium-webdriver'
+require 'capybara/cucumber'
+
+Capybara.default_driver = :selenium_chrome
 
 Before do
-  @driver = Selenium::WebDriver.for :chrome
+  @driver = Capybara.current_session.driver.browser
   @driver.manage.window.maximize
 end
 
@@ -14,45 +16,38 @@ end
 APP_BASE_URL = 'http://127.0.0.1:8080/checklist'
 
 Given('I am on the checklist page') do
-  @driver.get(APP_BASE_URL)
+  visit APP_BASE_URL
 end
 
 Given('I see the following checklist items:') do |table|
-    table.hashes.each do |item|
-      document_id = case item['Document']
-                    when 'Passport' then 'passport'
-                    when 'Proof of Employment' then 'employment'
-                    when 'Proof of Residential Address' then 'address'
-                    when 'Proof of Tax Residency' then 'tax'
-                    when 'Proof of Mobile Ownership' then 'mobile'
-                    else
-                      raise "Unknown document: #{item['Document']}"
-                    end
-  
-      begin
-        checkbox = @driver.find_element(:id, document_id)
-      rescue Selenium::WebDriver::Error::NoSuchElementError => e
-        raise "Element with id '#{document_id}' not found on the checklist page."
-      end
-  
-      if item['Status'] == 'checked'
-        checkbox.click unless checkbox.selected?
-      else
-        checkbox.click if checkbox.selected?
-      end
+  table.hashes.each do |item|
+    document_id = case item['Document']
+                  when 'Passport' then 'passport'
+                  when 'Proof of Employment' then 'employment'
+                  when 'Proof of Residential Address' then 'address'
+                  when 'Proof of Tax Residency' then 'tax'
+                  when 'Proof of Mobile Ownership' then 'mobile'
+                  else
+                    raise "Unknown document: #{item['Document']}"
+                  end
+    checkbox = find(:css, "##{document_id}")
+    if item['Status'] == 'checked'
+      checkbox.set(true) unless checkbox.checked?
+    else
+      checkbox.set(false) if checkbox.checked?
     end
   end
-  
+end
 
 Then('the Next button should be grayed out') do
-  button = @driver.find_element(:id, 'nextButton')
-  expect(button.enabled?).to be_falsey
-  expect(button.attribute('class')).to include('grayed-out')
+  button = find_button('nextButton')
+  expect(button[:disabled]).to be_truthy
+  expect(button[:class]).to include('grayed-out')
 end
 
 Then('the Next button should be clickable and red') do
-  button = @driver.find_element(:id, 'nextButton')
-  expect(button.enabled?).to be_truthy
-  expect(button.attribute('class')).to include('clickable')
-  expect(button.attribute('class')).to include('red')
+  button = find_button('nextButton')
+  expect(button[:disabled]).to be_falsey
+  expect(button[:class]).to include('clickable')
+  expect(button[:class]).to include('red')
 end
