236e7471e4a1bdacf91577089ecdca2ae9a0e2f1
Samuel Lam <samuellam123456@hotmail.com>
2024-07-09 14:27:18 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get 'about', to: 'home#about'
+  get 'test', to: 'home#test'
   # Defines the root path route ("/")
   root "home#about"
 end
