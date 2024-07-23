a72412fdebc87476aeaeb975ab3223486147edd1
moulikpare <139928243+moulikpare@users.noreply.github.com>
2024-07-24 00:51:09 +0800

    OTP Page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/taxres', to: 'home#taxres'
   get '/mobileno', to: 'home#mobileno'
   get '/login', to: 'home#login'
+  get '/otp', to: 'home#otp'
 
   get '/proof_of_identity', to: 'upload#proof_of_identity'
   get '/proof_of_employment', to: 'upload#proof_of_employment'
