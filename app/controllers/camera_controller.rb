843d60cddf0e68cc4104d42f0fbd2b10c3e31e90
Qaisel <ernesttanweiyan@gmail.com>
2024-07-24 22:36:10 +0800

    added new views

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -4,6 +4,7 @@ class CameraController < ApplicationController
   def process(response)
     image_data = params[:image_data].split(",")[1]
     result = generate_content(Base64.decode64(image_data),"Check if the uploaded image is blurry or not an legitimate phone bill, and provide the results as boolean integer key-value pairs for both conditions. Additionally, extract the phone number from the phone bill and return it as a separate key-value pair, the key values should be blurry,legitimate and phone_number,fill in empty slots with null. Do not use JSON/python format for the output")
+    puts result
     result = result["candidates"][0]["content"]["parts"][0]["text"].split("\n")[0,3]
     hash = {}
     result.each do |line|
@@ -13,7 +14,7 @@ class CameraController < ApplicationController
       value = case value
               when 1 , '1' , "True" , "true", ' 1', '1 '
                 1
-              when 0 , '0' , "False" , "false", ' 0', '0 '
+              when 0 , '0' , "False" , "false", ' 1', '1 '
                 0
               else
                 value
