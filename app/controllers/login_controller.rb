3f23865c0e5089441cebcc8ec8f01bf26ae47678
Samuel Lam <samuellam123456@hotmail.com>
2024-08-03 13:21:18 +0800

    minor update

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -16,12 +16,30 @@ class LoginController < ApplicationController
           redirect_to generate_otp_path
         end
       else
-        puts "cant find user in db"
-        flash[:alert] = 'Invalid name, phone number, or password'
-        render :login
+        puts "cant find user in db, creating new user"
+        new_user = User.create(user_params)
+        if new_user.persisted?
+          session[:user_id] = new_user.id
+          session[:phone_number] = params[:phone_number]
+          redirect_to generate_otp_path
+        else
+          flash[:alert] = 'Failed to create new user'
+          render :login
+        end
       end
     else
       render :login
     end
+  end
+
+  private
+
+  def user_params
+    {
+      full_name: params[:full_name],
+      phone_number: params[:phone_number],
+      password: params[:password],
+      application_status: 'pending'
+    }
   end
 end
