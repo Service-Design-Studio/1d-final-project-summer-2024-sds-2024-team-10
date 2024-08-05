865ee48182f9683f322ca34df95f93383540c76a
Qaisel <ernesttanweiyan@gmail.com>
2024-08-06 01:06:37 +0800

    Finished data transfer

--- app/javascript/controllers/webcam_identity.js
+++ app/javascript/controllers/webcam_identity.js
@@ -1,13 +1,101 @@
-import { startup, takepicture, identity } from './function_identity.js';
-let temp;
-// Initialize the camera functionality
-document.addEventListener('DOMContentLoaded', startup, false);
+(function() {
+    var width = 320; // We will scale the photo width to this
+    var height = 0; // This will be computed based on the input stream
 
-// Access the `identity` variable after taking a picture
-document.getElementById('startbutton').addEventListener('click', function() {
-    takepicture();
-    temp = identity;
-    console.log(temp)
-}
-);
-export {temp};
+    var streaming = false;
+
+    var video = null;
+    var canvas = null;
+    var photo = null;
+    var startbutton = null;
+
+    function startup() {
+        nextButton = document.getElementById('nextButton');
+        video = document.getElementById('video');
+        canvas = document.getElementById('canvas');
+        photo = document.getElementById('photo');
+        startbutton = document.getElementById('startbutton');
+        nextButton.disabled = true;
+        navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false
+            })
+            .then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function(err) {
+                console.error("An error occurred: " + err);
+            });
+        video.addEventListener('canplay', function(ev) {
+            if (!streaming) {
+                height = video.videoHeight / (video.videoWidth / width);
+
+                if (isNaN(height)) {
+                    height = width / (4 / 3);
+                }
+
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
+            }
+        }, false);
+
+        startbutton.addEventListener('click', function(ev) {
+            takepicture();
+            ev.preventDefault();
+        }, false); 
+        clearphoto();
+    }
+
+    function clearphoto() {
+        var context = canvas.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+    }
+
+    function takepicture() {
+        var context = canvas.getContext('2d');
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+
+            var data = canvas.toDataURL('image/png');
+            sessionStorage.setItem("identity", data);
+            photo.setAttribute('src', data);
+            // Dynamically get the current URL
+            var baseURL = window.location.origin;
+            var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+            fetch(baseURL+'/camera/identity', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': csrfToken // Include CSRF token here
+                },
+                body: JSON.stringify({ image_data: data })
+            })
+            .then(response => response.json()
+            )
+            .then(data => {
+                console.log('Success:', data);
+                document.getElementById('result').innerText = data.result;
+                sessionStorage.setItem("extracted_identity", data.result);
+                nextButton.disabled = data.enable;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        } else {
+            clearphoto();
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', startup, false);
+
+})();
