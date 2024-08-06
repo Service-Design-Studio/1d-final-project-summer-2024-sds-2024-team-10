8b592ea721c8c0286575924e88fd05264675121a
Qaisel <ernesttanweiyan@gmail.com>
2024-08-07 00:50:58 +0800

    Data Transfer

--- app/javascript/controllers/mobile.js
+++ app/javascript/controllers/mobile.js
@@ -1,14 +1,30 @@
-function onReady() {
-    const camerabutton = document.getElementById('toCameraUpload');
-    camerabutton.addEventListener("click",goToCameraUpload);
+document.addEventListener('DOMContentLoaded', function() {
+    // Retrieve the base64 image data and text content from sessionStorage
+    let data = sessionStorage.getItem("mobile");
+    let extracted_identity = sessionStorage.getItem("extracted_mobile");
+    var button = document.getElementById('nextButton');
+    if (button) {
+      button.addEventListener('click', function() {
+        window.location.href = '/camera/tax';
+      });
+    }
+    // Check if image data exists
+    if (data) {
+        // Select the photo element
+        let photo = document.getElementById('photo');
+        // Set the src attribute of the photo element with the base64 data
+        photo.setAttribute('src', data);
+    } else {
+        console.log('No image data found in sessionStorage');
     }
 
-function goToCameraUpload(){
-    window.location.assign('/camera/mobile');
-}
-
-if (document.readyState !== "loading") {
-    onReady(); // Or setTimeout(onReady, 0); if you want it consistently async
-} else {
-    document.addEventListener("DOMContentLoaded", onReady);
-}
+    // Check if text content exists
+    if (extracted_identity) {
+        // Select the result element
+        let text = document.getElementById('result');
+        // Set the text content of the result element
+        text.innerText = extracted_identity;
+    } else {
+        console.log('No extracted identity data found in sessionStorage');
+    }
+});
