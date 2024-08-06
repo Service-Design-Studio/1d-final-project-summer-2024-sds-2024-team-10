d362c085642ab9d4e81eb9692b137456ff42757d
moulikpare <139928243+moulikpare@users.noreply.github.com>
2024-08-07 04:18:55 +0800

    OCR to Form

--- app/javascript/controllers/identity.js
+++ app/javascript/controllers/identity.js
@@ -1,3 +1,12 @@
+function convertRubyToJsonString(rubyString) {
+    // Replace Ruby hash rocket => with colon :
+    let jsonString = rubyString.replace(/=>/g, ':');
+    
+    // Replace single quotes with double quotes (if necessary)
+    jsonString = jsonString.replace(/'/g, '"');
+  
+    return jsonString;
+  }
 document.addEventListener('DOMContentLoaded', function() {
     // Retrieve the base64 image data and text content from sessionStorage
     let data = sessionStorage.getItem("identity");
@@ -22,8 +31,20 @@ document.addEventListener('DOMContentLoaded', function() {
     if (extracted_identity) {
         // Select the result element
         let text = document.getElementById('result');
+        let docutype = document.getElementById('docutype');
+        let nationality = document.getElementById('nationality');
+        let passport_expiry_date = document.getElementById('passport_expiry_date');
+        let name = document.getElementById('name');
+        let date_of_birth = document.getElementById('docutype');
+        console.log(extracted_identity);
+        temp = JSON.parse(convertRubyToJsonString(extracted_identity));
         // Set the text content of the result element
-        text.innerText = extracted_identity;
+        docutype.value = temp["document_type"];
+        nationality.value = temp["nationality"];
+        passport_expiry_date.value = temp["passport_expiry_date"];
+        name.value = temp["name"];
+        gender.value = temp["gender"];
+        date_of_birth.value = temp["date_of_birth"];
     } else {
         console.log('No extracted identity data found in sessionStorage');
     }
