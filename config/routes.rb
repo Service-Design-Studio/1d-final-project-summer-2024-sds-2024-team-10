e8a629191f4173dc813338223111513ecf616a47
moulikpare <139928243+moulikpare@users.noreply.github.com>
2024-08-07 10:23:33 +0800

    OCR Layout

--- config/routes.rb
+++ config/routes.rb
@@ -63,9 +63,6 @@ Rails.application.routes.draw do
 
   get '/general_information', to: 'customer_info#general_information'
 
-
-  get '/general_information', to: 'customer_info#general_information'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
